f7ddcdedfcbcc0bd3645f2dfb041cd4366c29cd6
Anjali121234 <ahlawatanjali317@gmail.com>
2025-06-22 14:20:14 -0700

    change2

--- src/components/Events.js
+++ src/components/Events.js
@@ -11,11 +11,11 @@ function Events() {
             <div className="logoimg">
             <img id="img1" src={logo} width="80%" />
            
-            <p id="p1">A film is-or should be-more like msuic than like fiction. It should be a progression of moods and feelings. The theme, What's behind the  emotion, the meaning,all that comes later?</p>
+            <p id="p1">"A film is-or should be-more like msuic than like fiction. It should be a progression of moods and feelings. The theme, What's behind the  emotion, the meaning,all that comes later?"</p>
             </div>
         <div className='about'>
           <p id="p3"> What is Cinema Club ?</p>
-         <p id="p2">A common place for all the movie freaks to discuss their hearts out! To explore the intricacies of the cinematic world. To be a part of a community of movie lovers.
+         <p id="p2">A common place for all the movie freaks to discuss their hearts out! To explore the intricacies of the cinematic world. Cinema Club organises screenign of latest movies at Convocation Hall,with a size of around 1800 seats with the  best projector in the city.These Screenings (which are free of cost,by the way) are met with much fanfare in a Convo bottled down to its full capacity.
          </p>
          <div className='imgcards'>
           <img src={logo}  width="150px" alt="" />
