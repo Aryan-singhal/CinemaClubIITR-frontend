057856b103a8e1407259968943a22639186ab671
Anjali121234 <ahlawatanjali317@gmail.com>
2025-06-22 14:08:29 -0700

    updated about us

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,24 +1,35 @@
 import React from 'react'
 import './Events.css'
+import logo from './logo.png'
 
 function Events() {
   return (
     <div className='events-container'>
+       <p className='p3h'>About Us</p>
         <div className="events-content">
-            <p className='p3h'>What is cinema club</p>
-            <p>A common place for all the movie freaks to discuss their hearts out! To explore the intricacies of the cinematic world.</p>
-            <p>A film is-or should be-more like msuic than like fiction. It should be a progression of moods and feelings. The theme, What's behind the  emotion, the meaning,all that comes later?</p>
-        </div>
-        <div className="imgcards">
-            <div className="crd1">hi1 jef jeg jhfs hjgfjs jdgshfgsdhfg jhgf jhjh fs fjhsd gjf</div>
-            <div className="crd2">hi2 jef jeg jhfs hjgfjs jdgshfgsdhfg jhgf jhjh fs fjhsd gjf </div>
-            {/* <div className="crd3"></div> */}
-            <div className="crd4">hi3 jef jeg jhfs hjgfjs jdgshfgsdhfg jhgf jhjh fs fjhsd gjf</div>
-            <div className="crd5">hi4 jef jeg jhfs hjgfjs jdgshfgsdhfg jhgf jhjh fs fjhsd gjf</div>
-        </div>
-      
-    </div>
-  )
-}
+           
+            <div className="logoimg">
+            <img id="img1" src={logo} width="80%" />
+           
+            <p id="p1">A film is-or should be-more like msuic than like fiction. It should be a progression of moods and feelings. The theme, What's behind the  emotion, the meaning,all that comes later?</p>
+            </div>
+        <div className='about'>
+          <p id="p3"> What is Cinema Club ?</p>
+         <p id="p2">A common place for all the movie freaks to discuss their hearts out! To explore the intricacies of the cinematic world. To be a part of a community of movie lovers.
+         </p>
+         <div className='imgcards'>
+          <img src={logo}  width="150px" alt="" />
+          <img src={logo}  width="150px" alt="" />
+         <img src={logo}  width="150px" alt="" />
+          </div>
+         </div>
+       
+        
+        
+      </div>
+    </div>  
+    
+  )}
+
 
 export default Events
